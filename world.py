23608b51ec2a4db75b532686b4b4a08f28ed163a
Christofer Odén <bei.oden@gmail.com>
2013-03-23 22:21:41 +0100

    Somewhat functioning world and player classes

--- world.py
+++ world.py
@@ -11,38 +11,138 @@ class World:
     def __getitem__(self, index):
         return self.tiles[index]
 
+    def is_passable(self, x, y):
+        x, y = int(x), int(y)
+        return self[x][y] == 0
 
-class Player:
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Rect:
+    def __init__(self, x, y, w, h):
+        self.x = float(x)
+        self.y = float(y)
+        self.w = float(w)
+        self.h = float(h)
+
+    def min_x(self):
+        return self.x
+
+    def min_y(self):
+        return self.y
+
+    def max_x(self):
+        return self.x + self.w
+
+    def max_y(self):
+        return self.y + self.h
+
+    def center(self):
+        return Point(self.max_x() / 2, self.max_y() / 2)
+
+    def left_center(self):
+        return Point(self.min_x(), self.max_y() / 2)
+
+    def right_center(self):
+        return Point(self.max_x(), self.max_y() / 2) 
+
+    def top_center(self):
+        return Point(self.max_x() / 2, self.min_y())
+
+    def bottom_center(self):
+        return Point(self.max_x() / 2, self.max_y())
+
+
+class Player(Rect):
     speed = 0.1
+    jump_force = 1.0
+    width = 1./48
+    height = 1./48
 
     def __init__(self, start_x, start_y):
-        self.x = float(start_x)
-        self.y = float(start_y)
+        Rect.__init__(self, start_x, start_y,
+                Player.width, Player.height)
         self.velx = 0
         self.vely = 0
         self.forcex = 0
         self.forcey = 0
+        self.jumping = False
 
-    def handle_input(input):
-        """Takes key presses from network"""
-        if key_pressed:
-            if key_left:
+    def handle_key(self, action, key):
+        """
+        Takes key presses from network
+
+        action - if the key was pressed or released
+        key    - what key
+
+        """
+
+        if action == 'pressed':
+            if key == 'left':
                 self.forcex += Player.speed
-            if key_right:
-                self.forcey += -Player.speed
-        if key_released:
-            if key_left:
+            if key == 'right':
+                self.forcex += -Player.speed
+            if key == 'space':
+                if not self.jumping:
+                    self.forcey = jump_force
+                    self.jumping = True
+
+        elif action == 'released':
+            if key == 'left':
                 self.forcex -= Player.speed
-            if key_right:
+            if key == 'right':
                 self.forcex -= -Player.speed
 
-    def update(self):
+    def update(self, world):
         """Update physics on object"""
         self.velx += self.forcex
         self.vely += self.forcey
         self.x += self.velx
         self.y += self.vely
+        print "x=%.2f y=%.2f xv=%.2f yv=%.2f" % (self.x, self.y,
+                self.velx, self.vely)
 
+        if self.jumping == True:
+            self.jumping = False
+            self.forcey -= Player.jump_force
+
+        x, y = self.x, self.y
+        width, height = self.width, self.height
+
+        # Check if top center is in wall
+        top_cent = self.top_center()
+        if not world.is_passable(top_cent.x, top_cent.y):
+            # Snap to bottom of world tile
+            self.y = float(int(self.y + 1))
+            self.vely = 0
+
+        # Check if bottom center is in wall
+        bot_cent = self.bottom_center()
+        if not world.is_passable(bot_cent.x, bot_cent.y):
+            self.y = float(int(self.y))
+            self.vely = 0
+
+        # Check if left center is in wall
+        l_cent = self.left_center()
+        if not world.is_passable(l_cent.x, l_cent.y):
+            self.x = float(int(self.x + 1))
+            self.velx = 0
+
+        # Check if right center is in wall
+        r_cent = self.right_center()
+        if not world.is_passable(r_cent.x, r_cent.y):
+            self.x = float(int(self.x))
+            self.velx = 0
+
+    def get_state(self):
+        state = {}
+        state['pos'] = (self.x, self.y)
+        state['vel'] = (self.velx, self.vely)
+        state['jumping'] = (self.jumping)
 
 def _create_test_world():
     """FIXME: Temporary function for creating test world"""
@@ -70,3 +170,8 @@ if __name__ == "__main__":
         for x in range(world.width):
             sys.stdout.write("%d" % world[x][y])
         sys.stdout.write('\n')
+
+    player = Player(2, 2)
+
+    while(True):
+        player.update(world)
